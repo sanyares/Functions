99c1b72b3fd586370f2701b0359a1144b77b68cb
sanyares <sanyares@yandex.ru>
2022-11-09 19:39:41 +0300

    POWER

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,9 +1,44 @@
 ﻿#include<iostream>
 using namespace std;
+int Sum(int a=0, int b=0); // Прототип функции(Объявление функции)
+int Difference(int a, int b);
+int Product(int a, int b);
+double Quotient(int a, int b);
+
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Functions" << endl;
+	int a, b;
+	cout << "Введите два числа:"; cin >> a >> b;
+	int c = Sum(a, b);// вызов функции - Function call
+	cout << a << "+ " << b << "=" << c << endl;
+	cout << a << "- " << b << "=" << Difference(a, b) << endl;
+	cout << a << "* " << b << "=" << Product(a, b) << endl;
+	cout << a << "/ " << b << "=" << Quotient(a, b) << endl;
+}
+int Sum(int a, int b) //реализация функции (определение функции - function definition)
+состоит из заголовка и тела функции
+
+{
+	 int sum = a + b;
+	return sum;
+}
+
+int Difference(int a, int b)
+{
+	return a - b;
 
 }
+
+int Product(int a, int b)
+{
+	return a * b;
+}
+
+double Quotient(int a, int b)
+{
+	return (double) a / b;
+}
+
---
+++ Power/Power.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+
+
+using namespace std;
+
+
+double power(double a, int n);
+
+#define POWER
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef POWER
+	double a;
+	int n;
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введитете показатель степени: "; cin >> n;
+	cout << power(a, n) << endl;
+#endif // POWER
+}
+	double power(double a, int n)
+	{
+		if (n == 0)return 1;
+		else if (n > 0)return a * power(a, n - 1);
+		else return 1 / a * power(a, n + 1);
+		
+	}
